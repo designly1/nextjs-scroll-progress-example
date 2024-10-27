0b6d5defee150df923dfc8784bc4dbbf9ae3cac9
Jay Simons <jay@designly.biz>
2024-10-27 16:15:05 -0500

    init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import { Link } from 'nextjs13-progress';
+import GitHubButton from './GitHubButton';
 
 import { usePathname } from 'next/navigation';
 
@@ -18,7 +19,12 @@ function Button({ href, children }: ButtonProps) {
 	const isActive = currentPath === href;
 
 	return (
-		<Link className={`flex font-bold items-center justify-center text-xl md:text-2xl ${isActive ? 'bg-secondary text-base-300' : 'bg-base-300 text-secondary'}`} href={href}>
+		<Link
+			className={`flex font-bold items-center justify-center text-xl md:text-2xl ${
+				isActive ? 'bg-secondary text-base-300' : 'bg-base-300 text-secondary'
+			}`}
+			href={href}
+		>
 			{children}
 		</Link>
 	);
@@ -26,9 +32,13 @@ function Button({ href, children }: ButtonProps) {
 
 export default function Header() {
 	return (
-		<header className="h-20 w-full grid grid-cols-2">
-			<Button href="/">Document Scroll</Button>
-			<Button href="/div">Div Scroll</Button>
-		</header>
+		<>
+			<header className="h-20 w-full grid grid-cols-2 fixed z-20">
+				<Button href="/">Document Scroll</Button>
+				<Button href="/div">Div Scroll</Button>
+			</header>
+			<div className="h-20" />
+			<GitHubButton />
+		</>
 	);
 }
